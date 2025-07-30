e49055052818c17d73258eedc6462a92018fcf88
esbykov <esbykov@gmail.com>
2025-07-30 14:19:32 +0300

    Создаем главное меню проекта, метку и кнопку

--- Dogs.py
+++ Dogs.py
@@ -1,9 +1,25 @@
 import requests
 from tkinter import *
+from tkinter import messagebox as mb
 from PIL import Image, ImageTk
 from io import BytesIO
 
 
+def show_image():
+    image_url = get_random_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.image = img
+        except requests.RequestException as e:
+            mb.showerror("Ошибка", f"Не удалось загрузить изображение: {e}")
+                             
 
 
 window = Tk()
